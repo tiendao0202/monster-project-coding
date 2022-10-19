107e5cb27d3ef97672f55a61f8b97ec716039194
eastrd <e0t3rx@gmail.com>
2022-10-20 08:27:15 +1100

    Deploy React Codes to Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Component } from 'react';
 
-function App() {
+import { useState, useEffect } from 'react';
+import CardList from './components/card-list/card-list.component';
+import './App.css';
+import SearchBox from './components/search-box/search-box.component';
+import LightAnimation from './components/light-animation/light-animation.component';
+
+
+
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+  
+
+
+  useEffect(() => {
+   
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+  
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+  
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='app-title'>Jasmine's Monsters Collection</h1>
+      <SearchBox
+        className='monsters-seach-box'
+        placeholder='Search Monsters'
+        onChangeHandler={onSearchChange} />   
+     
+  
+      <CardList monsters={filteredMonsters} />
+      <LightAnimation />
+      
     </div>
   );
+
+
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField:''
+//     };
+//   }
+//   componentDidMount() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) => this.setState(() => {
+//         return {monsters: users}
+//       },
+        
+//       ));
+//   }
+//   onSearchChange = (event) => {
+//       console.log(event.target.value);           
+//       const searchField = event.target.value.toLocaleLowerCase();          
+//       this.setState(
+//         () => {
+//           return { searchField};
+//         });
+//   }
+  
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filterMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+  
+
+//     return (
+//       <div className='App'>
+//         <h1 className='app-title'>Jasmine's Monsters Collection</h1>
+//         <SearchBox
+//           className='monsters-seach-box'
+//           placeholder='Search Monsters'
+//           onChangeHandler={onSearchChange} />       
+       
+//         <CardList monsters={filterMonsters} />
+        
+//         <LightAnimation />
+
+//       </div>
+//     );
+
+     
+//   }
+  
+// }
 
 export default App;
---
+++ src/components/light-animation/light-animation.component.jsx
@@ -0,0 +1,23 @@
+
+import './light-animation.styles.css';
+
+
+const LightAnimation =()=> (
+ 
+      <div >
+        <div className='light x1'></div>
+        <div className='light x2'></div>
+        <div className='light x3'></div>
+        <div className='light x4'></div>
+        <div className='light x5'></div>
+        <div className='light x6'></div>
+        <div className='light x7'></div>
+        <div className='light x8'></div>
+        <div className='light x9'></div>
+      </div>
+
+
+    );    
+        
+    
+export default LightAnimation;
